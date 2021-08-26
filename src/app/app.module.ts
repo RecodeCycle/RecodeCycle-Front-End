b129b06d313ebecf78dbb40313dda06cac8b723e
Garrido10 <fernando.garrido98@gmail.com>
2021-08-26 12:29:00 -0300

    Implementando Rodape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { MenuInicialComponent } from './menu-inicial/menu-inicial.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    MenuInicialComponent
+    MenuInicialComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
