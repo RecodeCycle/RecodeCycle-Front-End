e14897f3c3c18b439bc741cccd438efd2e96d483
Vinicius <ciusivinicius@hotmail.com>
2021-09-08 13:20:12 -0300

    Implementação do post e get de postagens

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,16 +1,82 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Categoria } from '../model/Categoria';
+import { Postagem } from '../model/Postagem';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
+  postagem: Postagem = new Postagem();
+  listaPostagens: Postagem[];
 
-  constructor(private router: Router) { }
+  categoria: Categoria = new Categoria();
+  listaCategoria: Categoria[];
+  idCategoria: number;
 
-  ngOnInit(): void {
+  user: Usuario = new Usuario();
+  idUser = environment.id;
+
+  constructor(
+    private router: Router,
+    private postagemService: PostagemService,
+    private categoriaService: CategoriaService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    if (environment.token == '') {
+      this.router.navigate(['/entrar']);
+    }
+
+    this.getAllTemas();
+    this.getAllPostagens();
   }
 
+  getAllTemas() {
+    this.CategoriaService.getAllTema().subscribe((resp: Categoria[]) => {
+      this.listaCategoria = resp;
+    });
+  }
+
+  findByIdCategoria() {
+    this.CategoriaService.getByIdCategoria(this.idCategoria).subscribe((resp: Categoria) => {
+      this.categoria = resp;
+    });
+  }
+
+  getAllPostagens() {
+    this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp;
+    });
+  }
+
+  findByIdUser() {
+    this.authService.getByIdUser(this.idUser).subscribe((resp: Usuario) => {
+      this.user = resp;
+    });
+  }
+
+  publicar() {
+    this.categoria.id = this.idCategoria;
+    this.postagem.categoria = this.categoria;
+
+    this.user.id = this.idUser;
+    this.postagem.usuario = this.user;
+
+    this.postagemService
+      .postPostagem(this.postagem)
+      .subscribe((resp: Postagem) => {
+        this.postagem = resp;
+        alert('Postagem realizada com sucesso!');
+        this.postagem = new Postagem();
+        this.getAllPostagens();
+      });
+  }
 }
